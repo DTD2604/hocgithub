95f05acaf4d2123cb7bebbacab17e8265f896a4f
Dowin <thangproh96@gmail.com>
2023-06-22 17:28:11 +0700

    add consoleapp1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Tests
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+        }
+    }
+
+}
+
+/*namespace HelloWorld
+{
+    public class HelloWorldTests
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+        }
+    }
+}*/
